9ebf7328f05759ab57f7e934ff0ac2b84c608c16
enugulameghana4567 <meghanaenugula@gmail.com>
2026-04-14 13:51:23 +0530

    updated api url

--- mongodb_project_node/src/pages/FormPage.jsx
+++ mongodb_project_node/src/pages/FormPage.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API = "http://localhost:5000/api/responses";
+const API = "https://mongodb-response-sheet.onrender.com";
 
 const initialState = {
   name: "",
